0ee39749f68d87ad5d696ca1f4ec660440643741
Nishad Chaoji <89744466+XXGdragonXX@users.noreply.github.com>
2025-03-24 10:19:25 +0000

    added the dataset and query information

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -8,7 +8,79 @@ def main():
     st.title("📈 Sales Forecasting Dashboard")
     st.markdown("### Forecasting April Sales using an Ensemble Model")
     st.write("This app predicts sales using a combination of **Moving Average** and **Time Series** models.")
+    st.write("The final forecast is calculated using an **Ensemble Model** that combines both predictions.")
+    st.write("Use the sidebar to adjust the model weightage and click the button to run the model.")
+    st.write("Dataset link : https://www.kaggle.com/datasets/devarajv88/walmart-sales-dataset/data ")
+    st.write("SQL Query to add Date column : ") 
+    st.code("""
+    UPDATE purchase_data
+    SET purchase_date = DATE_ADD('2025-01-01', INTERVAL FLOOR(1 + (RAND() * 90)) DAY);
+    """, language="sql")
 
+    st.write("SQL Query to create monthly sale for each brand : ")
+    st.code("""
+        CREATE VIEW purchase_lvl_data AS 
+        select 
+        pd.Product_Category as `Brand`,
+        pd.purchase_date as `Date`,
+        sum(pd.Purchase) as `Total_Sale`
+        from purchase_data pd 
+        GROUP BY 
+        pd.Product_Category,
+        pd.purchase_date;
+
+        -- Create the second view: monthly_sale_jan
+
+        CREATE VIEW monthly_sale_jan AS
+        select 
+        Brand,
+        sum(Total_Sale) as `Jan_Sale`
+        from purchase_lvl_data
+        WHERE 
+        MONTH(Date) = 1
+        GROUP BY Brand;
+
+        -- create veiw for sale for february
+        CREATE VIEW monthly_sale_feb AS
+        select 
+        Brand,
+        sum(Total_Sale) as `feb_Sale`
+        from purchase_lvl_data
+        WHERE 
+        MONTH(Date) = 2
+        GROUP BY Brand;
+
+        -- create veiw for sale for March
+        CREATE VIEW monthly_sale_march AS
+        select 
+        Brand,
+        sum(Total_Sale) as `March_Sale`
+        from purchase_lvl_data
+        WHERE 
+        MONTH(Date) = 2
+        GROUP BY Brand;
+
+
+        -- Create the final table
+        CREATE TABLE FINAL_TABLE 
+        SELECT 
+        jan.Brand as `Brand`,
+        jan.Jan_Sale as `Jan_Sale`,
+        feb.feb_Sale as `Feb_SALE`,
+        march.March_Sale as `March_Sale`
+
+        from 
+        monthly_sale_jan jan
+        JOIN
+        monthly_sale_feb feb
+        ON
+        jan.Brand = feb.Brand
+        JOIN
+        monthly_sale_march march 
+        ON 
+        jan.Brand = march.Brand;
+
+    """, language="sql")
     # Load Data
     data = pd.read_csv("final_data.csv")
 
